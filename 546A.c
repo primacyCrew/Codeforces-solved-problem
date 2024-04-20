43ed89cb1095a6d7b863c168b509980c808ca6f4
Md. Anowar Hosain <166140031+primacyCrew@users.noreply.github.com>
2024-04-20 22:49:43 +0600

    Soldier and Bananas

---
+++ 546A.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int k,n,w,x,y,z;
+    scanf("%d%d%d",&k,&n,&w);
+    x=w*k+w*w*k;
+    x=x/2;
+    y=x-n;
+    z==0;
+    if(x>n)
+        printf("%d\n",y);
+    else
+        printf("%d\n",z);
+    return 0;
+}
